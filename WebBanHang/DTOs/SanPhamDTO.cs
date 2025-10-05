06612e30f6a09a74229fe5ce249929e85ce9aaa9
Kendy205 <thanhtung002zzz@gmail.com>
2025-10-05 09:09:45 +0700

    Sua layoutUser

---
+++ WebBanHang/DTOs/SanPhamDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace WebBanHang.DTOs
+{
+    public class SanPhamDTO
+    {
+        public string TenSP { get; set; }
+        public string HinhAnh { get; set; }= string.Empty;
+        public decimal Gia { get; set; }
+    }
+}
--- WebBanHang/Program.cs
+++ WebBanHang/Program.cs
@@ -1,8 +1,10 @@
+using WebBanHang.FileUpload.IFileUpload;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddTransient<IBufferedFileUploadService,BufferedFileUploadService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
